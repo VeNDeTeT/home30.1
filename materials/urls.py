c1885bdcc9af7be4ffcc693cae35e4929318a598
Александр <ryasantsev.a.r@gmail.com>
2026-03-10 11:08:35 +0300

    namespace

--- materials/urls.py
+++ materials/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 from . import views
 
+app_name = "materials"
+
 router = DefaultRouter()
 router.register(r"courses", views.CourseViewSet)
 
