1888a5d7d0849c91b2f052b5f3ccc99480ab9c35
Александр <ryasantsev.a.r@gmail.com>
2026-04-11 00:46:54 +0300

    33 Celery доработка

--- config/celery.py
+++ config/celery.py
@@ -1,8 +1,17 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 app = Celery("config")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
+# Периодическая задача блокировки неактивных пользователей
+app.conf.beat_schedule = {
+    "deactivate_inactive_users_daily": {
+        "task": "users.tasks.deactivate_inactive_users",
+        "schedule": crontab(hour=0, minute=0),  # каждый день в 00:00
+    },
+}
